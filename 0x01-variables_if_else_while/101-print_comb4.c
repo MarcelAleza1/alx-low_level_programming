c220ca55e77677ad9931fe47cfc78bcb6a4f3858
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-14 06:43:04 -0700

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,9 +23,10 @@ int main(void)
 	while (e < 10)
 	{
 		d = 0;
-		while (d < 10){
+		while (d < 10)
+		{
 			c = 0;
-			while ( c < 10)
+			while (c < 10)
 			{
 				if (c != d && d != e && e < d && d < c)
 				{
