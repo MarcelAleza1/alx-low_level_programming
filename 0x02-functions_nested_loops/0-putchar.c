7d921a4909bea3e466d2ac2230e40142bccf7210
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-15 07:40:17 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <holberton.h>
+/**
+ * main - main block
+ * Description: Write a program that prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+
+	int i =0;
+
+	while (i < 7)
+	{
+		_putchar(c[i]);
+
+		i++;
+	}
+	_putchar("\n");
+	return (0);
+}
