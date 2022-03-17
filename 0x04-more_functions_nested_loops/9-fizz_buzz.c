6abc9670e64b6cfb77e7b2aa4e3f65146dd1dec3
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-17 12:26:51 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - Fizz for multiple of 3 and Buzz for multiples of 5
+ * FizzBuzz for both
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
