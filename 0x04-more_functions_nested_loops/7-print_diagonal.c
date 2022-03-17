dc0a1143d47a0e39d314db98005d7df14c6e2458
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-17 11:01:50 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,22 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i = 0, k = 0;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\n');
-	}
+		while (k < n)
+			{
+				for (i = 0; i < k; i++)
+				{
+					_putchar(' ');
+				}
 
+				_putchar('\n');
+
+				k++;
+			}
+	}
 	else
 		_putchar('\n');
 }
