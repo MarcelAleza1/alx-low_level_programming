ff7ede43f63537e72a4b29edda319b8b607053a5
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-17 10:19:33 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times numbers from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	char i = 0;
+	while (i < 14)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
