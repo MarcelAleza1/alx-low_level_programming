90a9abe19658cd086cc498a9781e3bc248e67105
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-17 10:27:07 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 
 	while (i < 10)
 	{
-		for (n = 0; n<= 14; n++)
+		for (n = 0; n <= 14; n++)
 		{
 			c = n;
-			if ( > 9)
+			if (n > 9)
 			{
 				_putchar('1');
 				c = n % 10;
