ef90427defaa212f9f5cdd8b5a9de6e413c1fc3a
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-21 12:48:02 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - function to print an array
+ * @a: pointer that points to the array location
+ * @n: interger that store the length of the array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (int i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
