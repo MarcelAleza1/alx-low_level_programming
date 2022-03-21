e4c95a645247638a61b4b2aa06dddf6e3808e671
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-21 12:52:16 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
 	{
 		_putchar(a[i]);
 		_putchar(',');
 		_putchar(' ');
 	}
 	_putchar('\n');
+	}
 }
