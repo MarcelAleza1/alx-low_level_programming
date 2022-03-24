0e7f2250863a8b75809a2f41dc056e9bf6adaf00
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-24 13:16:35 -0700

    humm

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h"
+#include <stdio.h>
 
 /**
  * infinites_add - adds two numbers
@@ -22,7 +22,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		bg = c2;
 	if (size_r <= bg + 1)
-		retun (0);
+		return (0);
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
