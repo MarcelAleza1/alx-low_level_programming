27c9c97acd04cb41ec3c4f9950a196955caadab0
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-23 22:50:57 -0700

    str cpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compare 2 strings
+ * @s1: pointer to the first string
+ * @s2: Pointer to thhe second string
+ * Return: the diffence of length
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (s1 != '\0')
+		i++;
+	while (s2 != '\0')
+		j++;
+
+	return (i-j);
+}
