0ab5a8479482d6c54daf3c3cc873f0a3bbcc1140
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-23 23:20:48 -0700

    stc cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-
-	return (s1[i] - s2[j]);
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
