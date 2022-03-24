991bc0d8b9b9ba5b7b5b3925d665fd0eee3451a9
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-23 22:32:56 -0700

    dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,12 @@ char *_strncpy (char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j <= n)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[j] = src[j];
 		j++;
 	}
-	dest[i] = '\0';
+	dest[j] = '\0';
 
 	return (dest);
 }
