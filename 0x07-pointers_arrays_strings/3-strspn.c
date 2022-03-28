1f7b259ff04dfdc405d78d813904e60ea9c20100
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-28 08:56:41 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,9 +15,9 @@ unsigned int _strpn(char *s, char *accept)
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		for (j = 0; accept >= '\0'; j++)
+		for (j = 0; accept[j] >= '\0'; j++)
 		{
-			if (s + i == accept + j)
+			if (s[i] == accept[j])
 			{
 				c++;
 			}
