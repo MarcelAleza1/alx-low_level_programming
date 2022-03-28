553589c419b4b474e120f0edf9af19c885959ecd
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-28 10:29:12 -0700

    hum

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_memeset -function fills the first n bytes of the memory area
+ * *_memset -function fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  * @s: the momey address (memeory area)
  * @b: constant byte b
  * @n: first n bytes
  */
-char *_memeset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
