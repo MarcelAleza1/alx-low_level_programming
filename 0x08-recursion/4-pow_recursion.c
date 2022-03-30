969ce17588c93c26ae4ee0a22438c0a0096babb4
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-30 10:37:54 -0700

    x power to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to print x raised to power of y
+ *
+ * @x: integer
+ * @y: integer
+ * Descriptiom: return -1 if y < 0
+ * Return: x **y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y > 0)
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
+}
