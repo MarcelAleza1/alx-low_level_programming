b14dcdc1fc771522cebd5797a13b0d52249cb248
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-03-30 11:58:38 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int is_prime_number(int n)
 {
 	if (n == 0 || n == 1 || n < 0)
 		return (0);
-	return check_prime(n, 2);
+	return check_if_prime(n, 2);
 }
 
 /**
@@ -19,14 +19,14 @@ int is_prime_number(int n)
  * Return: 1 if prime and o if not prime
  */
 
-int check_prime(unsigned int n, unsigned int i);
+int check_if_prime(unsigned int n, unsigned int i)
 {
-	if (n % c == 0)
+	if (n % i == 0)
 	{
-		if (n == c)
+		if (n == i)
 			return (1);
 		else
 			return (0);
 	}
-	return check_prime(n, i + 1);
+	return check_if_prime(n, i + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,3 +13,4 @@ int check(char *s, int start, int end, int pair);
 int is_palindrome(char *s);
 int checker(char *s1, char *s2, int a, int b, int wildUsed);
 int wildcmp(char *s1, char *s2);
+int check_if_prime(unsigned int n, unsigned int increment);
