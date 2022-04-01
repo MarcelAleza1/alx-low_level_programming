e87d1d6027e0786879c9cb4475ecae0e957c3131
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-04-01 10:28:35 -0700

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+
+int main(int arg, char *argv[])
+{
+	int i, val = 1;
+
+	if (arg != 3)
+	{
+		printf("Error\n"):
+			return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		val *= atoi(argv[i]);
+	}
+	printf("%d\n", val);
+	return (0);
+}
