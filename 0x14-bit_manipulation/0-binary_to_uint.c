36ca30c220a98fa9a19b5fc2ebf968b5ba04535d
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-04-28 12:13:11 -0700

    int to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, b2 = 1; len >= 0; len--, b2 *=2)
+	for (len--, b2 = 1; len >= 0; len--, b2 *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
