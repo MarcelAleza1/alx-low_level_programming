ab4bb315940a9a31e9eedc06e79dfda2a86d707f
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-05-02 09:53:59 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can'nt read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
 
 	if (close(file2) < 0)
 	{
-		dprintf(STDERR_FILENO, "eRROR: Can't clode fd %d\n", file2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
 		exit(100);
 	}
 	if (close(file2) < 0)
